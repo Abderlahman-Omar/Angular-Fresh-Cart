da66db0df2ca7477d6ea8c58a48fb8a2986c5673
Abderlahman-Omar <159623837+Abderlahman-Omar@users.noreply.github.com>
2024-07-14 08:21:55 +0300

    number of cart items update

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -30,7 +30,10 @@ export class ProductDetailsComponent implements OnInit {
   }
   addToCart(productId: string) {
     this.cartService.addToCart(productId).subscribe({
-      next: (response) => console.log(response),
+      next: (response) => {
+        console.log(response);
+        this.cartService.numberOfCartItem.next(response.numOfCartItems);
+      },
       error: (error) => console.log(error),
     });
   }
